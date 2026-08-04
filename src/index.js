70e9428443395ec282e4d74f11f8a3ced4a8d664
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-08-04 20:52:34 +0530

    add middlewares and apiError, apiResponse, asyncHandler utilities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running at PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed! ", error);
+  });
